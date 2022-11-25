cf9404e9b6a371792832fdc49be5c4125725604b
tsai chen <505102470zhen@gmail.com>
2022-11-25 16:19:44 +0800

    no message

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,14 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class SiteController extends Controller
 {
-    public function index(){
-      return view('index');
+    public function index()
+    {
+        return view('index');
     }
-    public function shop(){
-      return view('shop');
+    public function shop()
+    {
+        return view('shop');
+    }
+    public function contact()
+    {
+        return view('contact');
+    }
+    public function confirmation()
+    {
+        return view('confirmation');
+    }
+    public function elements()
+    {
+        return view('elements');
     }
 }
