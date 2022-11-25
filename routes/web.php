a6c9c089f36d21d84992d8101cb2f2eed92f8005
Jason Lin <s872205@gmail.com>
2022-11-25 16:18:42 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,4 @@ Route::namespace ('App\Http\Controllers')->group(function () {
     Route::get('/shop', 'SiteController@index');
     Route::get('/product_details', 'SiteController@product_details');
 
-<<<<<<< HEAD
-=======
-Route::get('/','SiteController@index');
-Route::get('/shop','SiteController@shop');
->>>>>>> fa1a898695422f76ab278cf2f4bfc52aea40e2e3
 });
